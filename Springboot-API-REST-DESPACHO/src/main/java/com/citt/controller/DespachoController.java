2f6dfc2df8c5a868a74d478d1e5eb015031ebb57
Jarod Smith <lbriones.dev@gmail.com>
2025-04-13 21:24:58 -0400

    Feat/Agrega Swagger UI v2.7

--- Springboot-API-REST-DESPACHO/src/main/java/com/citt/controller/DespachoController.java
+++ Springboot-API-REST-DESPACHO/src/main/java/com/citt/controller/DespachoController.java
@@ -3,6 +3,8 @@ package com.citt.controller;
 import com.citt.exceptions.DespachoNotFoundException;
 import com.citt.persistence.entity.Despacho;
 import com.citt.persistence.services.DespachoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +14,16 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/despachos")
+@Tag(name = "Despacho", description = "Controlador para gestionar despachos")
 public class DespachoController {
 
     @Autowired
     private DespachoService despachoService;
 
+    @Operation(summary = "Crear un nuevo despacho")
     @PostMapping
     public ResponseEntity<Despacho> crearDespacho(
             @RequestBody Despacho despacho){
@@ -31,6 +36,7 @@ public class DespachoController {
         return ResponseEntity.created(location).body(despacho);
     }
 
+    @Operation(summary = "Actualizar un despacho existente")
     @PutMapping("/{idDespacho}")
     public ResponseEntity<Despacho> actualizarDespacho(
             @PathVariable Long idDespacho,
@@ -39,11 +45,13 @@ public class DespachoController {
         return ResponseEntity.ok(despachoActualizado);
     }
 
+    @Operation(summary = "Obtener todos los despachos")
     @GetMapping
     public ResponseEntity<List<Despacho>> getAllDespachos() {
         return ResponseEntity.ok(despachoService.findAllDespachos());
     }
 
+    @Operation(summary = "Obtener un despacho por ID")
     @GetMapping("/{idDespacho}")
     public ResponseEntity<Despacho> obtenerDespacho(
             @PathVariable Long idDespacho) throws DespachoNotFoundException {
@@ -51,6 +59,7 @@ public class DespachoController {
         return ResponseEntity.ok(despacho);
     }
 
+    @Operation(summary = "Eliminar un despacho por ID")
     @DeleteMapping("/{idDespacho}")
     public ResponseEntity<Void> eliminarDespacho(@PathVariable Long idDespacho) throws DespachoNotFoundException {
         despachoService.deleteDespacho(idDespacho);
