b4c9f9c3426eedfd13d3a176bae7b4b5607f90f3
Jarod Smith <lbriones.dev@gmail.com>
2025-04-18 09:30:27 -0400

    Fix/Agrega webMvcConfigurer solucionar problemas de CORS

--- Springboot-API-REST-DESPACHO/src/main/java/com/citt/SpringbootApiRestDespachoApplication.java
+++ Springboot-API-REST-DESPACHO/src/main/java/com/citt/SpringbootApiRestDespachoApplication.java
@@ -2,6 +2,9 @@ package com.citt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringbootApiRestDespachoApplication {
@@ -10,4 +13,15 @@ public class SpringbootApiRestDespachoApplication {
 		SpringApplication.run(SpringbootApiRestDespachoApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/api/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+		};
+	}
 }
