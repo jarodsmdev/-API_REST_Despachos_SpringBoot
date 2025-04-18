e194c94be930297641d06591379ae8b84e9e2e18
Jarod Smith <lbriones.dev@gmail.com>
2025-04-18 09:39:20 -0400

    Remove unused Lombok annotations from ErrorMessage class

--- Springboot-API-REST-DESPACHO/src/main/java/com/citt/exceptions/dto/ErrorMessage.java
+++ Springboot-API-REST-DESPACHO/src/main/java/com/citt/exceptions/dto/ErrorMessage.java
@@ -1,8 +1,6 @@
 package com.citt.exceptions.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import java.util.Map;
